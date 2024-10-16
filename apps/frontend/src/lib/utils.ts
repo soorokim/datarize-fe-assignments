69974af091f4af885ab350ef3566229e36bcbd6d
soorokim <rlatnfh99@gmail.com>
2024-10-16 18:11:04 +0900

    add: objectToSerachParams

--- apps/frontend/src/lib/utils.ts
+++ apps/frontend/src/lib/utils.ts
@@ -5,3 +5,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+// Object형태의 데이터를 입력받아 searchParam으로 변환한다.
+export const objectToSearchParams = (obj: { [key: string]: string | number }) =>
+  `?${Object.entries(obj)
+    .map(([key, val]) => (typeof val !== undefined ? `${key}=${val}` : false))
+    .filter(Boolean)
+    .join('&')}`;
